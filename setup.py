ee41f7a30801cf4b768c8492bb23c9ee89459371
Ted Lawless <lawlesst@gmail.com>
2012-06-26 12:01:40 -0700

    Setup redo.

--- setup.py
+++ setup.py
@@ -1,35 +1,95 @@
-import os
+"""
+Setup script adapted from Datadesk's softhyphen project:
+https://github.com/datadesk/django-softhyphen/blob/master/setup.py
+
+Which says: 
+Tricks lifted from Django's own setup.py and django_debug_toolbar.
+
+Still not sure why the templates install with this particular config
+and not with some of the others I tried.
+"""
+
+from distutils.core import setup
 from setuptools import setup, find_packages
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
+import os
+import sys
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+class osx_install_data(install_data):
+    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
+    # for this in distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
+    
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to the
+        # fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
 
-template_patterns = [
-    'templates/*.html',
-    'templates/*/*.html',
-    'templates/*/*/*.html',
-    ]
+if sys.platform == "darwin": 
+    cmdclasses = {'install_data': osx_install_data} 
+else: 
+    cmdclasses = {'install_data': install_data} 
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+django_dir = 'shibboleth'
+
+for dirpath, dirnames, filenames in os.walk(django_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+# Small hack for working with bdist_wininst.
+# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
+if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
+    for file_info in data_files:
+        file_info[0] = '\\PURELIB\\%s' % file_info[0]
 
 setup(
-    name = "django-shibboleth-remoteuser",
-    version = "1",
-    long_description = read('README.md'),
-    author = 'Ted Lawless',
-    author_email = 'tlawless@brown.edu',
-    url = 'https://github.com/Brown-University-Library/django-shibboleth-remoteuser',
-    packages = find_packages('src'),
-    include_package_data = True,
-    package_dir = {'': 'src'},
-    
-    install_requires = ['setuptools',
-                        'django>=1.3'],
-
-    classifiers = [
-        'Development Status :: 1 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-    ]
+      name = "django-shibboleth-remoteuser",
+      version='0.1',
+      long_description = read('README.md'),
+      author = 'Ted Lawless',
+      author_email = 'tlawless@brown.edu',
+      url = 'https://github.com/Brown-University-Library/django-shibboleth-remoteuser',
+      include_package_data = True,
+      packages=packages,
+      cmdclass = cmdclasses,
+      data_files=data_files,
+      include_package_data=True,
 )
+
+
