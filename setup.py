fbfe12721d2d5d7f73f721df6236209320d2925b
Ted Lawless <lawlesst@gmail.com>
2013-01-12 10:44:50 -0500

    Change version.

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
 
 setup(
       name = "django-shibboleth-remoteuser",
-      version='0.1',
+      version='0.2',
       long_description = read('README.md'),
       author = 'Ted Lawless',
       author_email = 'tlawless@brown.edu',
