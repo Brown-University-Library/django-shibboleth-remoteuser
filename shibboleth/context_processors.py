28049fda74830eee2af802a616353749ab2d1150
Ted Lawless <lawlesst@gmail.com>
2013-02-06 09:53:12 -0500

    Better logout link

--- shibboleth/context_processors.py
+++ shibboleth/context_processors.py
@@ -20,6 +20,9 @@ def logout_link(request, *args):
     e.g: https://school.edu/Shibboleth.sso/Login
     """
     from app_settings import LOGOUT_URL, LOGOUT_REDIRECT_URL
-    out = reverse('shibboleth:logout')
-    ll = "%s?target=%s" % (out, quote(LOGOUT_REDIRECT_URL))
+    #LOGOUT_REDIRECT_URL specifies a default logout page that will always be used when
+    #users logout from Shibboleth.
+    target = LOGOUT_REDIRECT_URL or quote(request.build_absolute_uri())
+    logout = reverse('shibboleth:logout')
+    ll = "%s?target=%s" % (logout, target)
     return { 'logout_link': ll }
