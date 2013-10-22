bd63c2091bc419a1ca8077cf10749e865354bca4
Ted Lawless <lawlesst@gmail.com>
2013-10-22 10:20:41 -0400

    Update docstring.

--- shibboleth/tests/middleware.py
+++ shibboleth/tests/middleware.py
@@ -5,11 +5,9 @@ For tests only.  Sample ShibbolethRemoteUserMiddleware class.
 from shibboleth.middleware import ShibbolethRemoteUserMiddleware
 
 class ShibTestMiddleware(ShibbolethRemoteUserMiddleware):
-    #def process_request(self, request):
-    #    super(ShibbolethRemoteUserMiddleware, self).process_request(request)
 
     def after_login(self, request, user):
         """
-        Create a user after login.
+        Assign user attributes from Shibboleth after login.
         """
         ShibbolethRemoteUserMiddleware.create_user(self, request, user)
