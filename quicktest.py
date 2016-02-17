ff92304976f21cf79f2023e78debf1884d4145f4
Ben Cail <benjamin_cail@brown.edu>
2016-02-17 09:09:38 -0500

    fix some whitespace

--- quicktest.py
+++ quicktest.py
@@ -64,13 +64,13 @@ class QuickDjangoTest(object):
             },
             INSTALLED_APPS = self.INSTALLED_APPS + self.apps,
             MIDDLEWARE_CLASSES = (
-                                        'django.contrib.sessions.middleware.SessionMiddleware',
-                                                'django.middleware.common.CommonMiddleware',
-                                                        'django.middleware.csrf.CsrfViewMiddleware',
-                                                                'django.contrib.auth.middleware.AuthenticationMiddleware',
-                                                                        'django.contrib.messages.middleware.MessageMiddleware',
-                                                                                'django.middleware.clickjacking.XFrameOptionsMiddleware',
-                                                                                    ),
+                    'django.contrib.sessions.middleware.SessionMiddleware',
+                    'django.middleware.common.CommonMiddleware',
+                    'django.middleware.csrf.CsrfViewMiddleware',
+                    'django.contrib.auth.middleware.AuthenticationMiddleware',
+                    'django.contrib.messages.middleware.MessageMiddleware',
+                    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+                ),
 	    ROOT_URLCONF = 'shib.urls',
         )
     
