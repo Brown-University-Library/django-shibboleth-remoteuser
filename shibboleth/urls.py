e253475d3d88bc47cda39197e249b4b53362e8e5
Brad Busenius <bbusenius@uchicago.edu>
2016-02-20 14:41:55 -0600

    Fixed deprecation warnings for Django 1.10.

--- shibboleth/urls.py
+++ shibboleth/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 
 from .views import ShibbolethView, ShibbolethLogoutView, ShibbolethLoginView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^login/$', ShibbolethLoginView.as_view(), name='login'),
     url(r'^logout/$', ShibbolethLogoutView.as_view(), name='logout'),
     url(r'^$', ShibbolethView.as_view(), name='info'),
-)
+]
