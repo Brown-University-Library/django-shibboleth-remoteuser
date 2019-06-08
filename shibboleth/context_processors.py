1fa0c0ccf9e7ab38ff88a044d128fe326c4ded4d
Marcos A. de S. Silva <chuckymass@gmail.com>
2019-06-08 11:07:25 -0300

    Makes it easy to create the login url

--- shibboleth/context_processors.py
+++ shibboleth/context_processors.py
@@ -4,26 +4,27 @@ except ImportError:
     from django.urls import reverse
 from django.utils.six.moves.urllib_parse import quote
 
+try:
+    from app_settings import LOGOUT_URL, LOGOUT_REDIRECT_URL, LOGIN_REDIRECT, LOGIN_URL_PARAMETER
+except ImportError:
+    from .app_settings import LOGOUT_URL, LOGOUT_REDIRECT_URL, LOGIN_REDIRECT, LOGIN_URL_PARAMETER
+
 def login_link(request):
     """
-    This assumes your login link is the Shibboleth login page for your server 
-    and uses the 'target' url parameter.
+    This assumes your login link is the Shibboleth login page for your server
+    and uses the 'target' url parameter by default.
     """
-    full_path = quote(request.get_full_path())
+    full_path = LOGIN_REDIRECT_URL or quote(request.get_full_path())
     login = reverse('shibboleth:login')
-    ll = "%s?target=%s" % (login, full_path)
+    ll = "%s?%s=%s" % (login, LOGIN_URL_PARAMETER, full_path)
     return { 'login_link': ll }
 
 def logout_link(request, *args):
     """
-    This assumes your login link is the Shibboleth login page for your server 
+    This assumes your login link is the Shibboleth login page for your server
     and uses the 'target' url parameter.
     e.g: https://school.edu/Shibboleth.sso/Login
     """
-    try:
-        from app_settings import LOGOUT_URL, LOGOUT_REDIRECT_URL
-    except ImportError:
-        from .app_settings import LOGOUT_URL, LOGOUT_REDIRECT_URL
     #LOGOUT_REDIRECT_URL specifies a default logout page that will always be used when
     #users logout from Shibboleth.
     target = LOGOUT_REDIRECT_URL or quote(request.build_absolute_uri())
