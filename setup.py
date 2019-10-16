6b3d247d8efb87b4603281c27dec39dbc49edfc2
Ben Cail <benjamin_cail@brown.edu>
2019-10-16 10:09:18 -0400

    version 0.11

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
 
 setup(
       name = "django-shibboleth-remoteuser",
-      version='0.11a2',
+      version='0.11',
       long_description = read('README.rst'),
       author = 'Brown University Library',
       author_email = 'bdr@brown.edu',
